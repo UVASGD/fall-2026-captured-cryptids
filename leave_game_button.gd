3b06ea116f7c442397714cb3fa3178b10802cadb
Enoch Tan <technological102@gmail.com>
2026-09-19 16:18:28 -0400

    parallax pt. 1

--- leave_game_button.gd
+++ leave_game_button.gd
@@ -8,5 +8,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_pressed() -> void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	#var tween = create_tween()
+	#tween.tween_property($CanvasLayer/ColorRect, "modulate:a", 1.0, 1.5).from(0.0)
+	#await tween.finished	
 	get_tree().quit()
 	pass
